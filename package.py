c5ff0b660cd33c476f58c3bd6809b1b9145aa39a
Dustin Luttrell <dustin@dluttrell.com>
2025-02-16 20:46:00 -0600

    fix package initialization order and string representation

--- package.py
+++ package.py
@@ -2,14 +2,14 @@
 
 class Package:
 
-    def __init__(self, package_id, address, city, state, zip_code, weight, deadline, special_notes):
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, special_notes):
         self.package_id = package_id
         self.address = address
         self.city = city
         self.state = state
         self.zip_code = zip_code
-        self.weight = weight
         self.deadline = deadline
+        self.weight = weight
         self.special_notes = special_notes
 
     def update_status(self, time_delta):
@@ -17,6 +17,6 @@ class Package:
 
     def __str__(self):
         return (f"Package ID: {self.package_id}, Address: {self.address}, City: {self.city}, State: {self.state},"
-                f" Zip Code: {self.zip_code}, Weight: {self.weight}, Deadline: {self.deadline},"
+                f" Zip Code: {self.zip_code}, Deadline: {self.deadline}, Weight: {self.weight},"
                 f" Special Notes: {self.special_notes}")
 
