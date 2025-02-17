38b0ba99b67c39696de415cbd5f5880943f3a01d
Dustin Luttrell <dustin@dluttrell.com>
2025-02-16 23:49:14 -0600

    feat: add create_truck function to initialize truck objects with default values

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import csv
+from datetime import datetime, time
+
 from package import Package
+from truck import Truck
 
 def load_package_list(package_list, package_table):
     """Loads package objects from list into a hash table"""
@@ -65,3 +68,25 @@ def calculate_distance(address1_index, address2_index, distance_list):
         return distance_list[address2_index][address1_index]
     else:
         return distance_list[address1_index][address2_index]
+
+def create_truck(truck_number):
+    """Creates a truck object with default values"""
+    # Default values for all trucks
+    # avg_speed = 18, truck_capacity = 16, starting_address = "4001 South 700 East", depart_time = "08:00:00"
+    # One of the trucks can't leave until 10:20 am since the delivery address for package #9 is wrong
+    avg_speed = 18
+    truck_capacity = 16
+    starting_address = "4001 South 700 East"
+    depart_time = datetime.strptime("08:00:00", "%H:%M:%S").time()
+    if truck_number is 1:
+        truck = Truck(avg_speed, truck_capacity, [2, 31, 5, 36, 6, 38, 39, 30, 35, 23, 26, 27, 1], 0,
+                      0, starting_address, depart_time)
+        return truck
+    elif truck_number is 2:
+        truck = Truck(avg_speed, truck_capacity, [10, 13, 20, 14, 40, 37, 29, 21, 17, 15, 25, 34, 22], 0,
+                      0, starting_address, depart_time)
+        return truck
+    elif truck_number is 3:
+        truck = Truck(avg_speed, truck_capacity, [33, 8, 12, 3, 24, 32, 16, 11, 19, 4, 28, 18, 7, 9], 0,
+                      0, starting_address, depart_time)
+        return truck
