dd15f186b5fc9b2dcf4f19eaa81127b34f98acfc
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-02-16 19:23:49 -0600

    create scheduler.py

---
+++ scheduler.py
@@ -0,0 +1,12 @@
+
+
+class DeliveryScheduler:
+
+    def __init__(self):
+        pass
+
+    def nearest_neighbor(self, address):
+        pass
+
+    def schedule_deliveries(self, trucks):
+        pass
