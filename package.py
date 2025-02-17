ff14cb4053612a34e2cc2ff138f30b0688c2cca4
Dustin Luttrell <dustin@dluttrell.com>
2025-02-16 21:39:01 -0600

    update Package class to replace special_notes with status in constructor and string representation

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 class Package:
 
-    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, special_notes):
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, status):
         self.package_id = package_id
         self.address = address
         self.city = city
@@ -10,7 +10,7 @@ class Package:
         self.zip_code = zip_code
         self.deadline = deadline
         self.weight = weight
-        self.special_notes = special_notes
+        self.status = status
 
     def update_status(self, time_delta):
         pass
@@ -18,5 +18,5 @@ class Package:
     def __str__(self):
         return (f"Package ID: {self.package_id}, Address: {self.address}, City: {self.city}, State: {self.state},"
                 f" Zip Code: {self.zip_code}, Deadline: {self.deadline}, Weight: {self.weight},"
-                f" Special Notes: {self.special_notes}")
+                f" Status: {self.status}")
 
