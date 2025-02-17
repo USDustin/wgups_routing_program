e0bb31c56ae9ab052bc4a002994b6ee57be485d1
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-02-16 19:23:28 -0600

    create distance_table.py

---
+++ distance_table.py
@@ -0,0 +1,15 @@
+
+
+class DistanceTable:
+
+    def __init__(self):
+        pass
+
+    def _read_distances(self):
+        pass
+
+    def get_distance(self, address1, address2):
+        pass
+
+    def __str__(self):
+        pass
