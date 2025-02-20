eebbb7fcc80512bb2c485f0105aa0bdcf7298794
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-02-20 10:27:39 -0600

    feat: add load_truck method to initialize packages based on truck_id

--- truck.py
+++ truck.py
@@ -16,6 +16,26 @@ class Truck:
         self.address = "4001 South 700 East"
         self.depart_time = "08:00:00"
 
+    def load_truck(self):
+        """
+        Manually loads the truck with packages based on the truck_id
+        """
+        match self.truck_id:
+            case 1:
+                # Truck 1 (Leaves at 8:00 AM, prioritizes early deadlines)
+                # 13 packages
+                self.packages = [1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40]
+
+            case 2:
+                # Truck 2 (Leaves at 9:05 AM, takes delayed packages & must-be-on-truck-2 packages)
+                # 8 packages
+                self.packages = [3, 6, 18, 25, 28, 32, 36, 38]
+
+            case 3:
+                # Truck 3 (Leaves after 10:20 AM, takes package #9 and remaining packages)
+                # 19 packages
+                self.packages = [2, 4, 5, 7, 8, 9, 10, 11, 12, 17, 21, 22, 23, 24, 26, 27, 33, 35, 39]
+
 
 #    def deliver_packages(self):
 #        pass
