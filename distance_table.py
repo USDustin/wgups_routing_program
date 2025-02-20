820f87ede5f38a0893080e8456388dc68a1378b8
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-02-20 10:18:21 -0600

    feat: move distance reading functionality into DistanceTable class

--- distance_table.py
+++ distance_table.py
@@ -6,7 +6,11 @@ class DistanceTable:
         pass
 
     def _read_distances(self):
-        pass
+        """Reads resources/distance-file.csv and returns a 2D list of distance data."""
+        with open(self.file, "r") as file:
+            csv_reader = csv.reader(file)
+            distance_list = list(csv_reader)
+            return distance_list
 
     def get_distance(self, address1, address2):
         pass
