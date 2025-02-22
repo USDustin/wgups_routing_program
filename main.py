25e16e3c73003e672f1faae1e6e416a590dff9cd
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-02-21 18:22:21 -0600

    fix: remove unnecessary blank lines in main function

--- main.py
+++ main.py
@@ -17,10 +17,8 @@ from datetime import datetime, time
 
 
 def main():
-
     distance_table = DistanceTable("resources/distance-file.csv")
     package_list = utils.read_packages_file("resources/package-file.csv")
-    package_table = HashTable()
     address_list = utils.read_address_file("resources/address-file.csv")
 
     package_table = HashTable()
@@ -29,7 +27,6 @@ def main():
     truck_1 = Truck(1)
     truck_2 = Truck(2)
     truck_3 = Truck(3)
-    truck_list = [truck_1, truck_2, truck_3]
 
     truck_1.load_truck()
     truck_2.load_truck()
@@ -39,7 +36,6 @@ def main():
     truck_1_route = scheduler.create_route(truck_1)
     truck_2_route = scheduler.create_route(truck_2)
     truck_3_route = scheduler.create_route(truck_3)
-
 
     # There are 3 trucks and 2 drivers
     # truck_3 can not leave until one of the others has returned, and it's after 10:20 AM
