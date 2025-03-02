fd327d3380e0398bea0eae01fb5d4500ebd1ddc4
Dustin Luttrell <52095131+USDustin@users.noreply.github.com>
2025-03-02 12:14:43 -0600

    refactor: add comments to main function for better code clarity

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ from datetime import datetime, time
 
 
 def main():
+    # Read the 3 csv files under resources and load in their data
     distance_table = DistanceTable("resources/distance-file.csv")
     package_list = utils.read_packages_file("resources/package-file.csv")
     address_list = utils.read_address_file("resources/address-file.csv")
 
+    # Create a package table and load it with the package data
     package_table = HashTable()
     utils.load_package_table(package_list, package_table)
 
+    # Create 3 trucks and manually load them with packages in an unoptimized order
     truck_1 = Truck(1)
     truck_2 = Truck(2)
     truck_3 = Truck(3)
@@ -37,6 +40,7 @@ def main():
     truck_2_route = scheduler.create_route(truck_2)
     truck_3_route = scheduler.create_route(truck_3)
 
+    # Load and deliver the packages in the optimized order
     # There are 3 trucks and 2 drivers
     # truck_3 can not leave until one of the others has returned, and it's after 10:20 AM
     scheduler.deliver_packages(truck_1, truck_1_route)
@@ -47,6 +51,7 @@ def main():
     truck_3.current_time = depart_time
     scheduler.deliver_packages(truck_3, truck_3_route)
 
+    # Create a command line interface and print the main menu
     truck_list = [truck_1, truck_2, truck_3]
     cli = CommandLineInterface(package_table, truck_list)
     cli.print_menu()
